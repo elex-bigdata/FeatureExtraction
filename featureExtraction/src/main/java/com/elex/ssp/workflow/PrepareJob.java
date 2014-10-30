2414c4915d2053e657c51506870cd41e6feb1876
wuzhongju <wuzhongju@126.com>
2014-10-30 10:48:27 +0800

    update

--- featureExtraction/src/main/java/com/elex/ssp/workflow/PrepareJob.java
+++ featureExtraction/src/main/java/com/elex/ssp/workflow/PrepareJob.java
@@ -41,10 +41,10 @@ public class PrepareJob extends Job{
 		stmt.execute("add jar " + Constants.UDFJAR);
 		stmt.execute("CREATE TEMPORARY FUNCTION qn AS 'com.elex.ssp.udf.Query'");
 		String preHql = " insert overwrite table log_merge partition(day='"+day+"') ";
-		String navHql = " (SELECT reqid,MAX(regexp_replace(uid,',','')) as uid,MAX(pid) as pid,MAX(ip) as ip,MAX(nation) as nation,MAX(ua) as ua,MAX(os) as os,MAX(width) as width,MAX(height) as height,COUNT(1) AS pv  FROM nav_visit WHERE DAY = '"+day+"' GROUP BY reqid )a ";
-		String imprHql = " (SELECT reqid,adid,MAX(time)as time,COUNT(uid) AS impr FROM ad_impression WHERE DAY='"+day+"' GROUP BY reqid,adid)b ";
+		String navHql = " (SELECT reqid,MAX(regexp_replace(uid,',','')) as uid,MAX(pid) as pid,MAX(ip) as ip,MAX(nation) as nation,MAX(ua) as ua,MAX(os) as os,MAX(width) as width,MAX(height) as height,1 AS pv  FROM nav_visit WHERE DAY = '"+day+"' GROUP BY reqid )a ";
+		String imprHql = " (SELECT reqid,adid,MAX(time)as time,1 AS impr FROM ad_impression WHERE DAY='"+day+"' GROUP BY reqid,adid)b ";
 		String clickHql = " (SELECT reqid,COUNT(1) AS click FROM ad_click WHERE DAY ='"+day+"' GROUP BY reqid)c ";
-		String searchHql = " (SELECT reqid,CONCAT_WS(':',collect_set(qn(keyword))) AS q,COUNT(uid) AS sv FROM search WHERE DAY='"+day+"' GROUP BY reqid)d ";
+		String searchHql = " (SELECT reqid,CONCAT_WS(':',collect_list(qn(keyword))) AS q,COUNT(uid) AS sv FROM search WHERE DAY='"+day+"' GROUP BY reqid)d ";
 		String hql = preHql+"SELECT b.reqid,a.uid,a.pid,a.ip,a.nation,a.ua,a.os,a.width,a.height,a.pv,b.adid,b.impr,b.time,c.click,d.q,d.sv FROM "+imprHql+"LEFT OUTER JOIN "+navHql+"ON a.reqid = b.reqid LEFT OUTER JOIN "+clickHql+"ON c.reqid = b.reqid LEFT OUTER JOIN "+searchHql+"ON d.reqid = b.reqid";
 		System.out.println("==================PrepareJob-logMerge-sql==================");
 		System.out.println(hql);
@@ -61,6 +61,9 @@ public class PrepareJob extends Job{
 				"select reqid,max(time),uid,max(pid),max(ip),max(nation),max(ua),max(os),max(width),max(height),max(pv),sum(impr),max(click),max(sv) " +
 				"from log_merge where day ='"+day+"' and uid is not null group by reqid,uid";
 		stmt.execute(hql);
+		System.out.println("==================PrepareJob-logMerge2-sql==================");
+		System.out.println(hql);
+		System.out.println("==================PrepareJob-logMerge2-sql==================");
 		stmt.close();
 		return 0;		
 	}
@@ -87,12 +90,12 @@ public class PrepareJob extends Job{
 		stmt.execute("add jar " + Constants.UDFJAR);
 		stmt.execute("CREATE TEMPORARY FUNCTION qs AS 'com.elex.ssp.udf.QuerySplit'");
 		String preHql = " insert overwrite table query_en2 partition(day='"+day+"') ";
-		String hql = preHql+" select reqid,uid,tab.col1,nation,max(pv),sum(impr),max(sv),max(click) from log_merge lateral view qs(query,':') tab as col1 " +
+		String hql = preHql+" select reqid,uid,tab.col1,nation,max(pv),sum(impr),1,max(click) from log_merge lateral view qs(query,':') tab as col1 " +
 				"where day ='"+day+"' and array_contains(array('in','us','pk','ph','gb','au','za','lk','ca','sg','sg','nz','ie','ng','gh','cm'),nation) and query is not null and nation is not null and uid is not null " +
 						"group by reqid,uid,tab.col1,nation";
-		System.out.println("==================PrepareJob-queryEnCollect-sql==================");
+		System.out.println("==================PrepareJob-queryEnCollect2-sql==================");
 		System.out.println(hql);
-		System.out.println("==================PrepareJob-queryEnCollect-sql==================");
+		System.out.println("==================PrepareJob-queryEnCollect2-sql==================");
 		stmt.execute(hql);
 		stmt.close();
 		return 0;
