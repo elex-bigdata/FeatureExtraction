c8df16398c7109cbf6dc404bf7fba18951b75c8e
wuzhongju <wuzhongju@126.com>
2015-01-09 15:04:05 +0800

    update

--- featureExtraction/src/main/java/com/elex/ssp/workflow/PrepareJob.java
+++ featureExtraction/src/main/java/com/elex/ssp/workflow/PrepareJob.java
@@ -59,7 +59,7 @@ public class PrepareJob extends Job{
 		String preHql = " insert overwrite table log_merge partition(day='"+day+"') ";
 		String navHql = " (SELECT reqid,MAX(regexp_replace(uid,',','')) as uid,MAX(pid) as pid,MAX(ip) as ip,MAX(nation) as nation,MAX(ua) as ua,MAX(os) as os,MAX(width) as width,MAX(height) as height,1 AS pv  FROM nav_visit WHERE DAY = '"+day+"' GROUP BY reqid )a ";
 		//String imprHql = " (SELECT reqid,adid,case when dt is null then 'default' else dt end as ndt,max(time) as time,1 AS impr FROM ad_impression WHERE DAY='"+day+"' group by reqid,adid,dt)b ";
-		String imprHql = " (SELECT reqid,cadid(adid,'','"+adid+"') AS adid,cdt(dt,'"+dt+"') AS ndt,1 AS impr,MAX(TIME) AS TIME FROM ad_impression WHERE DAY = '"+day+"' GROUP BY reqid,slot)b ";
+		String imprHql = " (SELECT reqid,cadid(adid,'"+adid+"') AS adid,cdt(dt,'"+dt+"') AS ndt,1 AS impr,MAX(TIME) AS TIME FROM ad_impression WHERE DAY = '"+day+"' GROUP BY reqid,slot)b ";
 		String clickHql = " (SELECT reqid,COUNT(1) AS click FROM ad_click WHERE DAY ='"+day+"' GROUP BY reqid)c ";
 		String searchHql = " (SELECT reqid,concatcolon(qn(keyword)) AS q,COUNT(uid) AS sv FROM search WHERE DAY='"+day+"' GROUP BY reqid)d ";
 		String hql = preHql+"SELECT b.reqid,a.uid,a.pid,a.ip,a.nation,a.ua,a.os,a.width,a.height,case when a.pv is null then 0 else a.pv end," +
